90f74b4ce4fb474ce54a1ea43e9e56f3f0bb00a0
Eugene Kos <mstrsid@live.com>
2021-09-27 20:05:31 +0300

    init, add pojo

---
+++ app/src/main/java/by/kos/getcrypto/CoinPriceListFragment.kt
@@ -0,0 +1,38 @@
+package by.kos.getcrypto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import by.kos.getcrypto.databinding.FragmentCoinPriceListBinding
+
+class CoinPriceListFragment : Fragment() {
+
+    private var _binding: FragmentCoinPriceListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentCoinPriceListBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/by/kos/getcrypto/pojo/COinPriceInfoRawData.kt
@@ -0,0 +1,11 @@
+package by.kos.getcrypto.pojo
+
+import com.google.gson.JsonObject
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class COinPriceInfoRawData(
+    @SerializedName("RAW")
+    @Expose
+    val coinPriceInfoJsonObject: JsonObject? = null
+)
